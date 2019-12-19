e2f0feff511ab5abb6e46d7d2b2a009f4b6fd295
Babatunde Woodard-James <bwoodardjames@gmail.com>
2019-12-18 19:45:36 -0500

    pushing more functional code to github

--- app-functional.js
+++ app-functional.js
@@ -2,24 +2,17 @@ console.log("it's alive") // Test to see if JS is loading correctly
 
 // Function to create a new gameboard evaluator. This is what will be evaluated for the win-lose scenario instead of the user interface, since an array of arrays will be easier to evaluate than a single array of 42 values. 
 let newGameBoard = () => {
-return [
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0]
-];
+    return [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ];
 }
 
-// Here are the global variables. Gameboard is only used within each game. gameStats.counter allows for the P1/P2 toggle. redCount and blackCount are used by each function to evaluate whether or not the player has a winning series of four (might get locally scoped). playerOneWin and playerTwo win will simply store boolean values to evaluate after the winning functions read.
-let gameBoard = newGameBoard();
-let counter = 0;
-let redCount = 0;
-let blackCount = 0;
-let playerOneWin = 0;
-let playerTwoWin = 0;
-
+// Here are the global variables. Gameboard is only used within each game. gameStats.counter allows for the P1/P2 toggle. gameStats.redCount and gameStats.blackCount are used by each function to evaluate whether or not the player has a winning series of four (might get locally scoped). gameStats.playerOneWin and playerTwo win will simply store boolean values to evaluate after the winning functions read.
 let gameStats = {
     gameBoard: newGameBoard(),
     counter: 0,
@@ -38,81 +31,14 @@ let slotIndex = () => {
             clickIndex = i;
         }
     }
-    console.log(clickIndex)
     return clickIndex;
 
 }
-// // This function sends the chip to the bottom of the column of where the user clicked.
-
-// let sinkToBottom = () => {
-//     let ind = slotIndex();
-//     let remainder = 5-ind%6;
-//     ind+=remainder;
-//     return ind;
-// }
-
-// // This function allows for the stacking to take place (also took a very long time to figure out; had to fix some other functions first). If there is already something there, then keep looking up the column until there is an empty space.
-
-// // *****NOTE: Need to make sure nothing happens when a top slot is chosen.
-// let setThePiece = () => {
-//     let num = sinkToBottom()
-//     while(slotList()[num].style.backgroundColor === 'red' || slotList()[num].style.backgroundColor === 'black') {
-//         if (num%6 === 0) {
-//             return null;
-//         }
-//         else {
-//             num--}
-//     }
-//     return num
-// }
-
-// // These two functions take the slot of setThePiece and turn it black or yellow. Also, it stores a value for the behind the scenes gameboard.
-// // ******NOTE: Need to add something to show who's turn it is
-// let userClickRed = () => {
-//     let theSlot = setThePiece()
-//     if (event.target.className === 'slot') {
-//         document.querySelectorAll('.slot')[theSlot].style.backgroundColor = 'red';
-//     }
-//     return [theSlot, 'R'];
-// }
-
-// let userClickBlack = () => {
-//     let theSlot = setThePiece()
-//     if (event.target.className === 'slot') {
-//         document.querySelectorAll('.slot')[theSlot].style.backgroundColor = 'black';
-//     }
-//     return [theSlot, 'B'];
-
-// }
-
-// // This function reads our globally defined gameStats.counter and sees whether or not it is even or odd. That is how red and black is toggled.
-// let toggleUser = () => {
-//     let theClick;
-//     if (gameStats.counter%2 === 0) {
-//         theClick = userClickRed();
-//     }
-//     else {
-//         theClick = userClickBlack();
-//     }
-//     return theClick;
-// }
-
-// // let toggleUserTurn = () => (gameStats.counter%2 === 0) ? userClick('Red') : userClick('Black')
-// // console.log(toggleUserTurn())
-
-// let amendGameboard = () => {
-//     let user = toggleUser();
-//     let newChip = user[1]
-//     let row = user[0]%6;
-//     let col = Math.floor(user[0]/6);
-//     gameBoard[row][col] = newChip;
-//     gameStats.counter++;
-// }
 
 let sinkToBottom = () => {
     let ind = slotIndex()
-    let remainder = 5-ind%6;
-    ind+=remainder;
+    let remainder = 5 - ind % 6;
+    ind += remainder;
     return ind;
 }
 
@@ -131,34 +57,20 @@ const setThePiece = () => {
     gameStats.gameBoard[row][col] = setColor()
     console.log(gameStats.gameBoard)
     gameStats.counter++
-    return [row,col]
+    return [row, col]
 }
 
 let setColor = () => (gameStats.counter % 2 === 0) ? 'R' : 'B'
 
-const convertToGameboard = (row,col) => row + col*6
+const convertToGameboard = (row, col) => row + col * 6
 
 const userClick = ([row, col]) => {
     let num = convertToGameboard(row, col)
-    console.log(num)
-    // for (let i = 0; i < 6; i++) {
-    //     for (let j = 0; j < 7; j++) {
-    //         if (gameStats.gameBoard[i][j] === 'R') {
-    //             console.log(`R located at row ${i} and column ${j}`)
-    //         }
-    //         // if (gameStats.gameBoard[i][j] === 'R') {
-    //         //     document.querySelectorAll('.slot')[num].style.backgroundColor = 'red'
-    //         // }
-    //         // else if (gameStats.gameBoard[i][j] === 'B') {
-    //         //     document.querySelectorAll('.slot')[num].style.backgroundColor = 'black'
-    //         // }
-    //     }
-    // }
     if (gameStats.gameBoard[row][col] === 'R') {
-        document.querySelectorAll('.slot')[num].style.backgroundColor = 'red'
+        resetColor(document.querySelectorAll('.slot')[num],'red')
     }
     else if (gameStats.gameBoard[row][col] === 'B') {
-        document.querySelectorAll('.slot')[num].style.backgroundColor = 'black'
+        resetColor(document.querySelectorAll('.slot')[num],'black')
     }
 }
 
@@ -166,121 +78,89 @@ const userClick = ([row, col]) => {
 // The main function for game play. This takes the input of the user and converts it to the gameboard in the console. It also increments the gameStats.counter one this takes place.
 // ******NOTE: Want the popup to appear as soon as someone wins
 let playTheGame = () => {
-    let winner = false //didSomeoneWin();
+    let winner = didSomeoneWin();
+    console.log(`Somebody won: ${winner}`)
     if (winner == true) {
         chooseWinner()
     }
     else {
         userClick(setThePiece())
     }
-    if (gameStats.counter >=42) {
+    if (gameStats.counter >= 42) {
         alert(`It's a tie!`)
     }
 }
 
 let chooseWinner = () => {
-    if (gameStats.counter%2 === 0) {
+    if (gameStats.counter % 2 === 0) {
         alert('Player 2 Wins!')
-        playerTwoWin++
-        document.querySelector('#p2score').innerText = playerTwoWin;
+        gameStats.playerTwoWin++
+        changeScoreboard(gameStats.playerTwoWin,'#p2score')
     }
     else {
         alert('Player 1 Wins!')
-        playerOneWin++
-        document.querySelector('#p1score').innerText = playerOneWin;
+        gameStats.playerOneWin++
+        changeScoreboard(gameStats.playerOneWin,'#p1score')
     }
 }
 
-let evalBoard = (j,k) => {
-    let i = gameBoard[j][k];
-    switch (i) {
+const changeScoreboard = (newData, changeField) => {
+    document.querySelector(changeField).innerText = newData
+}
+
+// This function is the evaluator fuction. It checks each slot to determine if it is red or black, and then adds it to an ongoing count 
+const checkBoard = (slot) => {
+    switch (slot) {
         case 0:
             break
-        case "R": 
-            redCount++
-            blackCount = 0;
-            if (redCount === 4) {
+        case 'R':
+            gameStats.redCount++
+            gameStats.blackCount = 0;
+            if (gameStats.redCount === 4) {
                 break
             }
-        case "B":
-            blackCount++
-            redCount = 0;
-            if (blackCount === 4) {
+            break
+        case 'B':
+            gameStats.redCount = 0;
+            gameStats.blackCount++;
+            if (gameStats.blackCount === 4) {
                 break
             }
     }
-    return [redCount, blackCount]
 }
 
-let resetcounter = (a,b) => {
-    a = 0;
-    b = 0
+const collectAndReset = (arr1,arr2) => {
+    arr1.push(gameStats.redCount)
+    arr2.push(gameStats.blackCount)
+    gameStats.redCount = 0;
+    gameStats.blackCount = 0;
 }
 
 // This is the first of four winning functions. This sees if there is a 4 pair across any of the rows
 let winAcross = () => {
-    let board;
     let redWin = [];
     let blackWin = [];
     for (let j = 5; j >= 0; j--) {
         for (let k = 0; k < 7; k++) {
-            // board = evalBoard(j,k)
-            // console.log(board)
-            let i = gameBoard[j][k];
-            if (i === 0) {
-                break}
-            if (i === "R") {
-                redCount++
-                blackCount = 0;
-                if (redCount === 4) {
-                    alert("Player 1 Wins!")
-                    break}
-                }
-            if (i === "B") {
-                blackCount++
-                redCount = 0;
-                if (blackCount === 4) {
-                    alert("Player 2 Wins!")
-                    break}
-            }
+            let i = gameStats.gameBoard[j][k]
+            checkBoard(i)
         }
-        redWin.push(board[0])
-        blackWin.push(board[1])
-        resetcounter(redCount,blackCount)
+        collectAndReset(redWin,blackWin)
     }
-    return fourTogether(redWin,blackWin)
+    return fourTogether(redWin, blackWin)
 }
-
 // This function tests to see if a player has won vertically 
 let winVertical = () => {
     let redWin = [];
     let blackWin = [];
     for (let i = 0; i < 7; i++) {
         for (let j = 5; j >= 0; j--) {
-            let k = gameBoard[j][i];
-            if (k === 0) {
-                break}
-            if (k === "R") {
-                redCount++
-                blackCount = 0;
-                if (redCount === 4) {
-                    alert("Player 1 Wins!")
-                    break}
-                }
-            if (k === "B") {
-                blackCount++
-                redCount = 0;
-                if (blackCount === 4) {
-                    alert("Player 2 Wins!")
-                    break}
-            } 
+            let k = gameStats.gameBoard[j][i];
+            checkBoard(k)
         }
-        redWin.push(redCount)
-        blackWin.push(blackCount)
-        redCount = 0;
-        blackCount = 0;
+        collectAndReset(redWin,blackWin)
     }
-    return fourTogether(redWin,blackWin)
+    return fourTogether(redWin, blackWin)
 }
 
 // This function will evaluate across the diagonal from bottom left to top right
@@ -290,33 +170,14 @@ let winNEtoSW = () => {
     let k = 0;
     for (let j = 0; j < 4; j = k) {
         for (let i = 5; i >= 2; i--) {
-            let m = gameBoard[i][j];
-            if (m === 0) {
-                break
-            }
-            if (m === "R") {
-                redCount++
-                blackCount = 0;
-                if (redCount === 4) {
-                    break
-                }
-            }
-            if (m === "B") {
-                blackCount++
-                redCount = 0;
-                if (blackCount === 4) {
-                    break
-                }
-            }
+            let m = gameStats.gameBoard[i][j];
+            checkBoard(m)
             j++
         }
         k++;
-        redWin.push(redCount)
-        blackWin.push(blackCount)
-        redCount = 0;
-        blackCount = 0;
+        collectAndReset(redWin,blackWin)
     }
-    return fourTogether(redWin,blackWin)
+    return fourTogether(redWin, blackWin)
 }
 
 // This function will evaluate across the diagonal from bottom right to top left
@@ -326,42 +187,21 @@ let winNWtoSE = () => {
     let k = 6;
     for (let j = 6; j > 2; j = k) {
         for (let i = 5; i >= 2; i--) {
-            let m = gameBoard[i][j];
-            if (m === 0) {
-                break
-            }
-            if (m === "R") {
-                redCount++
-                blackCount = 0;
-                if (redCount === 4) {
-                    alert("Player 1 Wins!")
-                    break
-                }
-            }
-            if (m === "B") {
-                blackCount++
-                redCount = 0;
-                if (blackCount === 4) {
-                    alert("Player 2 Wins!")
-                    break
-                }
-            }
+            let m = gameStats.gameBoard[i][j];
+            checkBoard(m)
             j--
         }
         k--;
-        redWin.push(redCount)
-        blackWin.push(blackCount)
-        redCount = 0;
-        blackCount = 0;
+        collectAndReset(redWin,blackWin)
     }
-    return fourTogether(redWin,blackWin)
+    return fourTogether(redWin, blackWin)
 }
 
 // This function takes all of the separate winning functions to see if any of them have flipped to true. It also keeps the scoreboard for how many times each player has won.
-let didSomeoneWin = () =>  (winAcross() || winVertical() || winNWtoSE() || winNEtoSW());
+let didSomeoneWin = () => (winAcross() || winVertical() || winNWtoSE() || winNEtoSW());
 
 // This function evaluates whether or not there are four in a row in any particular direction
-let fourTogether = (arr1,arr2) => {
+let fourTogether = (arr1, arr2) => {
     let red = arr1.some(int => int === 4);
     let black = arr2.some(int => int === 4);
     return (red || black);
@@ -371,34 +211,33 @@ let fourTogether = (arr1,arr2) => {
 let slotList = () => {
     return document.querySelectorAll('.slot')
 }
-
+// Generic function to reset the color of an element in an array
+const resetColor = (elem, color) => elem.style.backgroundColor = color
 
 // Function to start a new game, one of the three event listeners in my code. Clears the visual display and the gameboard in the background
 let newGame = () => {
-    for (let i in slotList()) {
-        slotList()[i].style.backgroundColor = 'rgb(0,153,219)'
-    }
-    gameBoard = newGameBoard();
+    gameStats.gameBoard = newGameBoard();
     gameStats.counter = 0;
-    return gameBoard;
+    slotList().forEach(elem => resetColor(elem,'rgb(0,153,219)'))
 }
+
+// Function to restart a session, equivalent to refreshing the page
+const resetSession = () => {
+    newGame()
+    gameStats.playerOneWin = 0
+    gameStats.playerTwoWin = 0
+    changeScoreboard(0,'#p1score')
+    changeScoreboard(0,'#p2score')
+}
+
 
 // *****The event listeners!*********
 
-// The first one takes the play the game function and applies it to anywhere in the container
+// The first one takes the play the game function and applies it to slot in the container
+slotList().forEach(slot => slot.addEventListener('click', playTheGame, true))
 
-// ******NOTE: I need it to only apply to any slot within the container div and not the full container div itself. I'm still trying to figure that out. 
-for (let slot in slotList()) {
-    slotList()[slot].addEventListener('click', playTheGame, true)
-}
 // The second event listener runs the new Game function to reset the gameStats.counter, clear the console gameboard, and resets the user interface.
 document.querySelector('.restart').addEventListener('click', newGame)
 
-
-
-
-
-
-
-
-
+// The third event listener runs the new Game function to reset the gameStats.counter, clear the console gameboard, and resets the user interface.
+document.querySelector('.newSession').addEventListener('click', resetSession)
---
+++ readme.md
@@ -0,0 +1,18 @@
+***Connect Four Documentation***
+
+Welcome to the Connect Four game 1.0!
+
+This game requires two players. Red moves first, and black moves second.
+
+Simply click anywhere on your column where you would like your chip to go. The first person to connect four slots vertically, horizontally, or diagonally wins the match!
+
+Ready to play? Click <a href="/">here</a>
+
+
+***Anticipated updates during next release***
+1) Change names of player 1 and player 2
+2) Have some type of animation to indicate whose turn it is and to show the winning combination
+3) Have an alert appear as soon as the winning combination is clicked
+4) Turn the alert into a modal
+5) Add simple AI to play against computer
+6) Make CSS responsive
